60eab84a94c7912f8b945bd59ab62201ad5fcc87
webshoten <weboshimashoten@gmail.com>
2024-05-30 21:42:43 +0900

    hjny

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,18 @@ export default HoCAuth(function Home(props: Props) {
   return (
     <>
       <Layout {...props}>
-        <div className="grid min-h-screen w-screen bg-gray-400 p-5 sm:p-10">
-          fffffs
-        </div>
+        <main className="col-span-4 grid gap-4 p-4 md:col-span-8">
+          <p>よやくです詳細をみるにはクリックしてください。</p>
+          <div className="grid grid-cols-2 gap-1 text-white sm:grid-cols-4">
+            <div className="bg-slate-800 p-4 text-center">One</div>
+            <div className="bg-slate-800 p-4 text-center">Two</div>
+            <div className="bg-slate-800 p-4 text-center">Three</div>
+            <div className="bg-slate-800 p-4 text-center">Four</div>
+            <div className="bg-slate-800 p-4 text-center">Five</div>
+            <div className="bg-slate-800 p-4 text-center">Six</div>
+            <div className="bg-slate-800 p-4 text-center">Seven</div>
+          </div>
+        </main>
       </Layout>
     </>
   )
